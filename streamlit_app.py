5e4c775937cb34d9d574d18b61939cfd0059ae46
Li <175231748+linkmodo@users.noreply.github.com>
2025-03-01 15:03:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.markdown(
     }
     /* Set the sidebar background to 90% opaque */
     [data-testid="stSidebar"] {
-        background-color: rgba(255, 255, 255, 0.9);
+        background-color: rgba(255, 255, 255, 0.2);
     }
     </style>
     """,
